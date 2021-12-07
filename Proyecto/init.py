60dc1680a912658bb6af7acc22d3262d59a69def
jose.quezada <jose.quezada@sansano.usm.cl>
2021-12-06 21:19:14 -0300

    init proyecto

---
+++ Proyecto/init.py
@@ -0,0 +1,16 @@
+
+
+# vehículo de cualquier tipo que pueda transitar por la calle
+# se asume que hay solo una persona en cada uno
+# peatones deberían ser una clase distinta o podría usar la misma no más?
+class vehiculo:
+    def _init_(self, masa, initLoc):
+        self.masa = masa
+        self.loc = initLoc
+
+# instancias de calles?
+
+
+
+def main():
+    print("a")
